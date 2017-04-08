c55e641180ba7187ec7b4ee79a0c00707a35406f
Peter Wang <peter@sitedisks.com.au>
2017-04-08 17:33:11 +1000

    app lib dot net core initial

---
+++ applib/Library.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class MyClass
+    {
+        public void PrintOut()
+        {    
+			System.Console.WriteLine("class lib print out by p.W");
+        }
+		
+		public string GetStr(){
+			return "return from lib";
+			
+		}
+    }
+}
---
+++ coreapp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ClassLibrary;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        
+			MyClass myC=new MyClass();
+			myC.PrintOut();
+			var str = myC.GetStr();
+			Console.WriteLine(str);
+			Console.WriteLine("Hello World!");
+        }
+    }
+}
