a887890682240531e227738bf54bc0d170da4594
Peter Wang <peter@sitedisks.com.au>
2017-09-27 22:03:13 +1000

    geolocation use REST API

--- Ionic/PinPiece/www/app/home/dashboardCtrl.js
+++ Ionic/PinPiece/www/app/home/dashboardCtrl.js
@@ -1,28 +1,17 @@
 (function(){
     'use strict';
 
-    pinpieceCtrl.controller('dashboardCtrl', ['$scope', '$state', '$cordovaGeolocation',
-        function($scope, $state, $cordovaGeolocation){
+    pinpieceCtrl.controller('dashboardCtrl', ['$scope', '$http', '$state', '$ionicLoading', '$cordovaGeolocation',
+        function($scope, $http, $state, $ionicLoading, $cordovaGeolocation){
 
-            var searchManager;
-            $scope.map = null;
+            var bingKey = 'AqlUSI0HFVVtDNrRHti87vFyhCeL34H4hOflM_xKWx4FeLIAROakEXwK0K4z3Pt6';
             $scope.coords = null;
+            $scope.addresses = [];
 
             $scope.init = init;
             $scope.newPost = newPost;
 
-            var posOptions = {timeout: 10000, enableHighAccuracy: false};
-            var geolocation = $cordovaGeolocation.getCurrentPosition(posOptions);
-            geolocation.then(function (position) {
-                $scope.coords = position.coords;
-                var lat  = position.coords.latitude
-                var long = position.coords.longitude
-              }, function(err) {
-                // error
-                console.log(err);
-              });
-          
-          
+           function watchMove(){
             var watchOptions = {timeout : 3000, enableHighAccuracy: false};
             var watch = $cordovaGeolocation.watchPosition(watchOptions);
             watch.then(null,
@@ -32,57 +21,79 @@
               },
               function(position) {
                 $scope.coords = position.coords;
-                var lat  = position.coords.latitude
-                var long = position.coords.longitude
+                var lat  = position.coords.latitude;
+                var long = position.coords.longitude;
+                
+                fillAddresses();
+
             });
+
+           }
+            
 
             function init(){
 
-                //Try loading the map every 100ms until it works.
-                var interval = setInterval(function(){
-                    try{
-                        $scope.map = new Microsoft.Maps.Map(document.getElementById('mapDiv'), {
-                            credentials: 'AqlUSI0HFVVtDNrRHti87vFyhCeL34H4hOflM_xKWx4FeLIAROakEXwK0K4z3Pt6',
-                            // center: new Microsoft.Maps.Location(51.50632, -0.12714),
-                            // mapTypeId: Microsoft.Maps.MapTypeId.aerial,
-                            // zoom: 10
-                        });
-                        reverseGeocode();
-                        clearInterval(interval);
-                    }catch(e){
-                    }
-                }, 100);
+                showLoading('location...');
+                var posOptions = {timeout: 10000, enableHighAccuracy: false};
+                var geolocation = $cordovaGeolocation.getCurrentPosition(posOptions);
+                geolocation.then(function (position) {
+                    $scope.coords = position.coords;
+                    var lat  = position.coords.latitude;
+                    var long = position.coords.longitude;
+
+                    fillAddresses();
+
+                  }, function(err) {
+                    // error
+                    console.log(err);
+                    hideLoading();
+                  });
+            
             }
             
             function newPost(){
-
                 $state.go('app.addpost');
             }
 
-            function reverseGeocode() {
-                //If search manager is not defined, load the search module.
-                if (!searchManager) {
-                    //Create an instance of the search manager and call the reverseGeocode function again.
-                    Microsoft.Maps.loadModule('Microsoft.Maps.Search', function () {
-                        searchManager = new Microsoft.Maps.Search.SearchManager($scope.map);
-                        reverseGeocode();
+            function fillAddresses(){
+                geolocationReverse($scope.coords).then(function(resp){
+                    var resourceSets = resp.data.resourceSets;
+                    angular.forEach(resourceSets, function(item){
+                        angular.forEach(item.resources, function(add){
+                            var address = add.address;
+                            var name = add.name;
+                            $scope.addresses.push({shortAddress: name, fullAddress: address});
+                        });
                     });
-                } else {
-                    var searchRequest = {
-                        location: $scope.map.getCenter(),
-                        callback: function (r) {
-                            //Tell the user the name of the result.
-                            alert(r.name);
-                        },
-                        errorCallback: function (e) {
-                            //If there is an error, alert the user about it.
-                            alert("Unable to reverse geocode location.");
-                        }
-                    };
-        
-                    //Make the reverse geocode request.
-                    searchManager.reverseGeocode(searchRequest);
-                }
+                    hideLoading();
+                }, function(err){
+                    // error
+                    console.log(err);
+                    hideLoading()
+                });
+            }
+
+            // services
+            function geolocationReverse(coords) {
+                var lat  = coords.latitude;
+                var lng = coords.longitude;
+                var params = lat + ',' + lng;
+                return $http({
+                    type: 'GET',
+                    url: 'http://dev.virtualearth.net/REST/v1/Locations/' + params + '?o=json&key=' + bingKey,           
+                    datatype: 'json',
+                    contentType: 'application/json;charset=utf-8'
+                });
+            }
+
+            function showLoading(message) {
+                $ionicLoading.show({
+                    template: '<ion-spinner></ion-spinner><p>' + message + '</p>'
+                });
+            }
+
+            function hideLoading() {
+                $ionicLoading.hide();
             }
 
         }]);
