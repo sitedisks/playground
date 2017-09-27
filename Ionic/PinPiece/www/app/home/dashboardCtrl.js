3a088dfb1c338c3f3e47a43eece3761b66a86a16
Peter Wang <peter@sitedisks.com.au>
2017-09-27 16:06:10 +1000

    geolocation

--- Ionic/PinPiece/www/app/home/dashboardCtrl.js
+++ Ionic/PinPiece/www/app/home/dashboardCtrl.js
@@ -10,11 +10,28 @@
             $scope.newPost = newPost;
 
             function init(){
+
+                if(navigator.geolocation){
+                    navigator.geolocation.getCurrentPosition(function(position){
+                        var dd =  "Latitude: " + position.coords.latitude +
+                        "<br>Longitude: " + position.coords.longitude;
+                    });
+                }
+                else{
+                    // get
+
+                }
+
+
+
                 //Try loading the map every 100ms until it works.
                 var interval = setInterval(function(){
                     try{
                         $scope.map = new Microsoft.Maps.Map(document.getElementById('mapDiv'), {
-                            credentials: 'AqlUSI0HFVVtDNrRHti87vFyhCeL34H4hOflM_xKWx4FeLIAROakEXwK0K4z3Pt6'
+                            credentials: 'AqlUSI0HFVVtDNrRHti87vFyhCeL34H4hOflM_xKWx4FeLIAROakEXwK0K4z3Pt6',
+                            center: new Microsoft.Maps.Location(51.50632, -0.12714),
+                            mapTypeId: Microsoft.Maps.MapTypeId.aerial,
+                            zoom: 10
                         });
                         clearInterval(interval);
                     }catch(e){
