89e108e06ce65645b20ebe61f4411e95b4ade1f8
Peter Wang <peter@sitedisks.com.au>
2017-09-27 15:04:28 +1000

    Bing map integration

--- Ionic/PinPiece/www/app/home/dashboardCtrl.js
+++ Ionic/PinPiece/www/app/home/dashboardCtrl.js
@@ -4,8 +4,24 @@
     pinpieceCtrl.controller('dashboardCtrl', ['$scope', '$state',
         function($scope, $state){
 
+            $scope.map = null;
+
+            $scope.init = init;
             $scope.newPost = newPost;
 
+            function init(){
+                //Try loading the map every 100ms until it works.
+                var interval = setInterval(function(){
+                    try{
+                        $scope.map = new Microsoft.Maps.Map(document.getElementById('mapDiv'), {
+                            credentials: 'AqlUSI0HFVVtDNrRHti87vFyhCeL34H4hOflM_xKWx4FeLIAROakEXwK0K4z3Pt6'
+                        });
+                        clearInterval(interval);
+                    }catch(e){
+                    }
+                }, 100);
+            }
+            
             function newPost(){
 
                 $state.go('app.addpost');
