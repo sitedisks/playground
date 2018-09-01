aa19c8fb17da57bcff685243fa6126bedc7239f2
Peter Wang <peter@sitedisks.com.au>
2018-09-01 18:51:26 +0800

    https://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000 Python教学001

--- Python/Prac/1.py
+++ Python/Prac/1.py
@@ -1,2 +1,15 @@
-print('%2d-%02d' % (3, 1))
-print('%.2f' % 3.1415926)
+import urllib.request
+req = urllib.request.Request("http://placekitten.com/g/600/400")
+response = urllib.request.urlopen(req)
+
+info = response.info()
+print(info)
+
+'''
+cat_img = res.read()
+
+with open('cat_600_400.jpg','wb') as f:
+    f.write(cat_img)
+
+'''
+
