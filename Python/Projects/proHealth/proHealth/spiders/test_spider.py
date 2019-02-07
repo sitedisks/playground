b4a2f9d41f42a864d43a8e81cfa9c7231c0a6681
Peter Wang <peter@sitedisks.com.au>
2019-02-07 21:43:18 +1100

    final updates test spider

--- Python/Projects/proHealth/proHealth/spiders/test_spider.py
+++ Python/Projects/proHealth/proHealth/spiders/test_spider.py
@@ -1,4 +1,5 @@
 import re
+import logging
 
 from scrapy.spiders import Spider
 from proHealth.items import ProhealthItem
@@ -8,12 +9,11 @@ class TestSpider(Spider):
     name = "testspider"
     start_urls = [
         "https://www.bestong.com.au/index.php?City=Melbourne&Category=6&page=1&ipp=All"]
-
+        
     all_companies = {}
     detail_page_url = "https://www.bestong.com.au/business_details.php?Tag="
 
     def parse(self, response):
-        #item = ProhealthItem()
         business_list = response.xpath('//p[contains(@class, "line_shade_")]')
 
         for business in business_list:
@@ -26,37 +26,44 @@ class TestSpider(Spider):
             self.all_companies[company_id] = {
                 "name": name, "tel": tel, "address": address}
 
-        print("#> Total business: ", len(self.all_companies))        
+        print("#> Total business: ", len(self.all_companies))
 
         for id in self.all_companies:
             details_link = self.detail_page_url + id
-            print("#> Open detail page: ", details_link)
+            logging.info("Open detail page:")
+            print(details_link)
             yield response.follow(details_link, self.parse_details)
 
     def parse_details(self, response):
-
-        print("#> Detail page ", response.url, " Opened.")
+        self.logger.info('logger: Parse function called on %s', response.url)
         def find_between(s, start, end):
             return (s.split(start))[1].split(end)[0]
 
         item = ProhealthItem()
 
-        key_id = response.url.split('Tag=')[1]
-        this_company = self.all_companies[key_id]
+        try:
+            key_id = response.url.split('Tag=')[1]
+            this_company = self.all_companies[key_id]
 
-        item['business_id'] = key_id
-        item['business_name'] = this_company["name"]
-        item['business_tel'] = this_company["tel"]
-        item['business_address'] = this_company["address"]
-        item['details_html'] = response.css('.details').get().replace(
-            "联系时请说明是在百事通网站看到的，谢谢。提及百事通，享受更多优惠。", "").strip()
+            item['business_id'] = key_id
+            item['business_name'] = this_company["name"]
+            item['business_tel'] = this_company["tel"]
+            item['business_address'] = this_company["address"]
+            item['details_html'] = response.css('.details').get().replace(
+                "联系时请说明是在百事通网站看到的，谢谢。提及百事通，享受更多优惠。", "").strip()
+            item['_tel'] = response.xpath(
+                '//a[contains(@href, "tel:")]/text()').get().strip()
+            item['_fax'] = find_between(
+                response.text, '传真：', '联络：').split('<br/>')[0].strip()
+            item['_contact'] = find_between(
+                response.text, '联络：', '地址：').split('<br/>')[0].strip()
+            item['_email'] = find_between(
+                response.text, '邮件：', '网站：').split('<br/>')[0].strip()
+            website = find_between(response.text, '网站：', '<br/>').strip()
+            if len(website) > 1:
+                item['_website'] = find_between(website, 'href="', '"')
 
-        item['_tel'] = response.xpath('//a[contains(@href, "tel:")]/text()').get().strip()
-        item['_fax'] = find_between(response.text, '传真：', '联络：').split('<br/>')[0].strip()
-        item['_contact'] = find_between(response.text, '联络：','地址：').split('<br/>')[0].strip()
-        item['_email'] = find_between(response.text, '邮件：', '网站：').split('<br/>')[0].strip()
-        website = find_between(response.text, '网站：','<br/>')
-        item['_website'] = find_between(website, 'href="','"')
+            yield item
 
-        yield item
-
+        except:
+            self.logger.warning("ERROR: Spider error processing")
