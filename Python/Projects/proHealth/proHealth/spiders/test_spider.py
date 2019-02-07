ebe41884954c8f1f5c50fafcd0ed5f2161287dd9
Peter Wang <peter@sitedisks.com.au>
2019-02-07 12:05:52 +1100

    slightly updates

--- Python/Projects/proHealth/proHealth/spiders/test_spider.py
+++ Python/Projects/proHealth/proHealth/spiders/test_spider.py
@@ -3,25 +3,44 @@ import re
 from scrapy.spiders import Spider
 from proHealth.items import ProhealthItem
 
+
 class TestSpider(Spider):
     name = "testspider"
-    start_urls=["https://www.bestong.com.au/index.php?City=Melbourne&Category=6&page=1&ipp=All"]
+    start_urls = [
+        "https://www.bestong.com.au/index.php?City=Melbourne&Category=6&page=1&ipp=All"]
+
+    all_companies = {}
+    detail_page_url = "https://www.bestong.com.au/business_details.php?Tag="
 
     def parse(self, response):
-        item = ProhealthItem()
+        #item = ProhealthItem()
         business_list = response.xpath('//p[contains(@class, "line_shade_")]')
 
         for business in business_list:
-            item['business_name'] = business.css('.company_name::text').get().strip()
-            item['business_id'] = business.css('.business_id::text').get().strip()
-            item['business_tel'] = business.xpath('.//a[contains(@href, "tel:")]/text()').get().strip()
-            item['business_address'] = business.css('.google_address::text').get().strip()
-            business_id = item['business_id'].replace("商家 ID: B", "").strip()
-            details_link = "https://www.bestong.com.au/business_details.php?Tag=" + business_id
+            name = business.css('.company_name::text').get().strip()
+            tel = business.xpath(
+                './/a[contains(@href, "tel:")]/text()').get().strip()
+            address = business.css('.google_address::text').get().strip()
+            company_id = business.css('.business_id::text').get().replace(
+                "商家 ID: B", "").strip()
+            self.all_companies[company_id] = {
+                "name": name, "tel": tel, "address": address}
+
+        for id in self.all_companies:
+            details_link = self.detail_page_url + id
             yield response.follow(details_link, self.parse_details)
-            yield item
-    
+
     def parse_details(self, response):
         item = ProhealthItem()
-        item['details_html'] = response.css('.details').get()
+
+        key_id = response.url.split('Tag=')[1]
+        this_company = self.all_companies[key_id]
+
+        item['business_id'] = key_id
+        item['business_name'] = this_company["name"]
+        item['business_tel'] = this_company["tel"]
+        item['business_address'] = this_company["address"]
+        item['details_html'] = response.css('.details').get().replace(
+            "联系时请说明是在百事通网站看到的，谢谢。提及百事通，享受更多优惠。", "").strip()
+
         yield item
