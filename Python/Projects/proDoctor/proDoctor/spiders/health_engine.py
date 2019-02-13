1a16ed01d790c85df3d6b84918de047ee920496b
Peter Wang <peter@sitedisks.com.au>
2019-02-14 10:36:00 +1100

    progressing on healthengine

--- Python/Projects/proDoctor/proDoctor/spiders/health_engine.py
+++ Python/Projects/proDoctor/proDoctor/spiders/health_engine.py
@@ -8,6 +8,8 @@ class HealthEngineSpider(scrapy.Spider):
     all_practitioners = {}
     all_practices = {}
 
+
+    counter = 0
     err = []
     start_urls = [
         base_url + '/find/chinese-medicine-practitioner/Australia/']
@@ -16,41 +18,53 @@ class HealthEngineSpider(scrapy.Spider):
     def parse(self, response):
 
         for section in response.css('div#search-results-area-main section.search-card-contain'):
+            # decide practice | practitioner
+            practice = section.xpath('@data-practice-id')
+            practitioner = section.xpath('@data-practitioner-id')
             try:
-                # decide practice | practitioner
-                practice = section.xpath('@data-practice-id')
-                practitioner = section.xpath('@data-practitioner-id')
-
+                
                 name = section.css('h2.search-main-title a::text').get()
                 link = section.css(
                     'h2.search-main-title a').xpath('@href').get()
                 phone = section.css(
                     'span.search-phone div.search-phone-more-tt h2')
-                phone = phone.xpath(
-                    './a[contains(@href, "tel:")]').xpath('@href').get()
-                phone = phone.replace("tel:", "")
+                if phone:
+                    phone = phone.xpath(
+                        './a[contains(@href, "tel:")]').xpath('@href').get()
+                    phone = phone.replace("tel:", "")
 
                 if practice:  # sid for practice (business)
                     sid = section.xpath('@data-practice-id').get()
-
                     self.all_practices[sid] = {
                         "type": "Practice", "name": name, "link": link, "phone": phone}
                 elif practitioner:  # pid for practitioner (doctor)
                     pid = section.xpath('@data-practitioner-id').get()
                     # via practitioner link we can get the pid and sid(location id)
-
                     self.all_practitioners[pid] = {
                         "type": "Practitioner", "name": name, "link": link, "phone": phone}
                 else:
                     print("Not practice or practitioner")
 
             except:
-                print("section analyse error")
+                if practice:
+                    self.err.append(section.xpath('@data-practice-id').get())
+                elif practitioner:
+                    self.err.append(section.xpath(
+                        '@data-practitioner-id').get())
 
-        print(self.all_practices)
+                print("section analyse error")
 
         next_page = response.css(
             'div.he-global-pagination div.he-global-paglinks a.page-next').xpath('@href').get()
 
-        # if next_page:
-        #     yield scrapy.Request(self.base_url+next_page, callback=self.parse)
+        if next_page:
+            yield scrapy.Request(self.base_url+next_page, callback=self.parse)
+        else:
+            print("Practice: " + str(len(self.all_practices)))
+            print("Practitioners: " + str(len(self.all_practitioners)))
+
+    def parse_practice(self,response):
+        pass
+
+    def parse_practitioner(self,response):
+        pass
