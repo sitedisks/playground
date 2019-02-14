960938c0dc1256bf16f9094fc7bc71f45340484a
Peter Wang <peter@sitedisks.com.au>
2019-02-15 09:27:53 +1100

    just format

--- Python/Projects/proDoctor/proDoctor/items.py
+++ Python/Projects/proDoctor/proDoctor/items.py
@@ -15,7 +15,7 @@ class ProdoctorItem(scrapy.Item):
     # heData_obj['practitionerData']['practitionerName']
     p_name = scrapy.Field()
     P_cname = scrapy.Field()
-    p_profession = scrapy.Field() #职业
+    p_profession = scrapy.Field()  # 职业
     # heData_obj['practitionerData']['gender']
     p_gender = scrapy.Field()
     # detail_obj['image']
@@ -23,16 +23,15 @@ class ProdoctorItem(scrapy.Item):
     p_languages = scrapy.Field()
     p_eduction = scrapy.Field()
     # heData_obj['practitionerData']['primarySpecialty']
-    p_specialties = scrapy.Field() #特色
+    p_specialties = scrapy.Field()  # 特色
     p_description = scrapy.Field()
- 
+
     # practice: s_
     s_id = scrapy.Field()  # practice id / location id
     s_clinic_name = scrapy.Field()
     s_location = scrapy.Field()
     s_phone = scrapy.Field()
     s_mobile = scrapy.Field()
-    
 
     # heData_obj['practitionerData']['state']
     s_add_state = scrapy.Field()
@@ -42,4 +41,3 @@ class ProdoctorItem(scrapy.Item):
     s_add_post = scrapy.Field()
     # detail_obj['address']['streetAddress']
     s_add_street = scrapy.Field()
-    
