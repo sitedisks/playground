d6c74f7e0651d81276627fb2f5484dd675668956
Peter Wang <peter@sitedisks.com.au>
2019-02-15 22:35:47 +1100

    practitioner extraction

--- Python/Projects/proDoctor/proDoctor/spiders/practitioner_page.py
+++ Python/Projects/proDoctor/proDoctor/spiders/practitioner_page.py
@@ -1,83 +1,16 @@
-import scrapy
 import json
+from scrapy.spiders import Spider
 
 from proDoctor.items import ProdoctorItem
 
 
-class HealthEngineSpider(scrapy.Spider):
-
-    name = 'healthenginespider'
-    base_url = 'https://healthengine.com.au'
-    all_practitioners = {}
-    all_practices = {}
-
-    counter = 0
-    err = []
+class PractitionerPageSpider(Spider):
+    name = "practitionerspider"
     start_urls = [
-        base_url + '/find/chinese-medicine-practitioner/Australia/']
-    # https://healthengine.com.au/find/chinese-medicine-practitioner/Australia/
+        "file:///Users/peterwang/Dev/GitHub/playground/Python/Projects/proDoctor/html/practitioner_FayDai.html"]
 
     def parse(self, response):
-
-        for section in response.css('div#search-results-area-main section.search-card-contain'):
-            # decide practice | practitioner
-            practice = section.xpath('@data-practice-id')
-            practitioner = section.xpath('@data-practitioner-id')
-            try:
-
-                name = section.css('h2.search-main-title a::text').get()
-                link = section.css(
-                    'h2.search-main-title a').xpath('@href').get()
-                phone = section.css(
-                    'span.search-phone div.search-phone-more-tt h2')
-                if phone:
-                    phone = phone.xpath(
-                        './a[contains(@href, "tel:")]').xpath('@href').get()
-                    phone = phone.replace("tel:", "")
-
-                if practice:  # sid for practice (business)
-                    sid = section.xpath('@data-practice-id').get()
-                    self.all_practices[sid] = {
-                        "type": "Practice", "name": name, "link": link, "phone": phone}
-                elif practitioner:  # pid for practitioner (doctor)
-                    pid = section.xpath('@data-practitioner-id').get()
-                    # via practitioner link we can get the pid and sid(location id)
-                    self.all_practitioners[pid] = {
-                        "type": "Practitioner", "name": name, "link": link, "phone": phone}
-                else:
-                    print("Not practice or practitioner")
-
-            except:
-                if practice:
-                    self.err.append(section.xpath('@data-practice-id').get())
-                elif practitioner:
-                    self.err.append(section.xpath(
-                        '@data-practitioner-id').get())
-
-                print("section analyse error")
-
-        next_page = response.css(
-            'div.he-global-pagination div.he-global-paglinks a.page-next').xpath('@href').get()
-
-        if next_page:
-            yield scrapy.Request(self.base_url+next_page, callback=self.parse)
-        else:
-            print("Practice: " + str(len(self.all_practices)))
-            print("Processing all practices! ...")
-
-            print("Practitioners: " + str(len(self.all_practitioners)))
-            print("Processing all practices! ...")
-            for pid, values in self.all_practitioners.items():
-                details_link = self.base_url + values['link']
-                self.logger.info("Open practitioner detail page")
-                yield response.follow(details_link, self.parse_practitioner)
-
-    def parse_practice(self, response):
-        pass
-
-    def parse_practitioner(self, response):
-        # url link: /chinese-medicine-practitioner/wa/floreat/yolanda-shi/p42269?location=60505
-        self.logger.info('logger: Parse function called on %s', response.url)
+        self.logger.info('Practitioner page open')
 
         def find_between(s, start, end):
             return (s.split(start))[1].split(end)[0]
--- Python/Projects/proDoctor/runner.py
+++ Python/Projects/proDoctor/runner.py
@@ -3,10 +3,11 @@ from scrapy.crawler import CrawlerProcess
 
 from proDoctor.spiders.health_engine import HealthEngineSpider
 from proDoctor.spiders.local_file import LocalSpider
+from proDoctor.spiders.practitioner_page import PractitionerPageSpider
 
 process = CrawlerProcess({
     'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
 }) 
 
-process.crawl(HealthEngineSpider)
+process.crawl(PractitionerPageSpider)
 process.start() # the script will block here until the crawling is finished
