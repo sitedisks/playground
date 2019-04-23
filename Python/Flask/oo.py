2f6d8db727a1a89c0d352346298770bed7e739d4
Peter Wang <peter@sitedisks.com.au>
2019-04-23 12:32:29 +0800

    Python3 oo.py

--- Python/Flask/oo.py
+++ Python/Flask/oo.py
@@ -1,9 +1,30 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    }, {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+
+@app.route('/todo/api/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'data': tasks})
+
+@app.route('/todo/api/tasks/<int:task_id>',methods=['GET'])
+def get_one_task(task_id):
+    return 'Hi task ' + str(task_id)
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
