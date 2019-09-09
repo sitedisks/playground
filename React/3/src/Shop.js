d705db63664c34ef20939c184a885aec09c7a549
Peter Wang <peter@sitedisks.com.au>
2019-09-09 18:21:17 +1000

    The working order https://www.youtube.com/watch?v=Law7wfdg_ls

--- React/3/src/Shop.js
+++ React/3/src/Shop.js
@@ -1,10 +1,12 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 
+
 // fetch : https://fortnite-api.theapinetwork.com/upcoming/get?authorization=e23f8d7474d9dabe9864e82c7d7b1b1b
 
 function Shop() {
 
+  //Similar to componentDidMount and componentDidUpdate 
   useEffect(()=>{
     fetchItems();
   },[]);
@@ -15,17 +17,15 @@ function Shop() {
     const data = await fetch('https://fortnite-api.theapinetwork.com/upcoming/get?authorization=e23f8d7474d9dabe9864e82c7d7b1b1b');
     
     const items = await data.json();
-    console.log(items.items);
-    //setItems(items.items);
+    console.log(items);
+    setItems(items.data);
   }
 
   return (
     <div className="App">
-      {
-        items.map(item=>(
-          <h1>{item.name}</h1>
-        ))
-      }
+     {items.map(item=>(
+       <h1 key={item.itemId}>{item.item.name}</h1>
+     ))}
     </div>
   ); 
 }
