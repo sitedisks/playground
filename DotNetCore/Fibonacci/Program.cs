e8361ecda2fdc2b198ea606a1346338e7fd13bbc
Peter Wang <peter@sitedisks.com.au>
2019-09-29 11:23:42 +1000

    organise the code

---
+++ DotNetCore/Fibonacci/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Fibonacci
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Fibonacci");
+            var fib = new FibonacciGenerator();
+            foreach (var item in fib.Generate(15))
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
