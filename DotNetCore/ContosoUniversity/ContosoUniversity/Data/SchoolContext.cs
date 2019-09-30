9e5302b06c601ae34b37dd0794631896c543f8e4
cwan40 <peter@sitedisks.com.au>
2019-09-30 18:54:26 +1000

    visual studio webapi entityframeworkcore

---
+++ DotNetCore/ContosoUniversity/ContosoUniversity/Data/SchoolContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Data {
+  
+    public class SchoolContext : DbContext { 
+        public SchoolContext(DbContextOptions<SchoolContext> options):base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+    }
+}
+
