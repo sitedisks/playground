0a70717104a836dc12b92477f638b342c82c2b51
Peter@Desktop6700 <peter@sitedisks.com.au>
2019-10-01 00:07:13 +1000

    check in from 6700Desktop

--- DotNetCore/myApi3Todo/Startup.cs
+++ DotNetCore/myApi3Todo/Startup.cs
@@ -1,15 +1,8 @@
-using System;
-using System.Reflection;
-using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using myApi3Todo.Models;
 using Microsoft.OpenApi.Models;
