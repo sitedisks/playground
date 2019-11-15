3a6d5fd0305f63c6b186eeff26c4624176b0ea24
Peter@Desktop6700 <peter@sitedisks.com.au>
2019-11-15 17:50:25 +1100

    practise the entityframework

---
+++ DotNetCore/Fibonacci/Model/User.cs
@@ -0,0 +1,24 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Fibonacci.Model{
+    [Table("user")]
+    public class User
+    {
+        [Column("userId")]
+        public int Id { get; set; }
+        [Column("userName")]
+        public string UserName { get; set; }
+        [Column("alias")]
+        public string Alias { get; set; }
+        [Column("email")]
+        public string Email { get; set; }
+        public DateTime? SignupDate { get; set; }
+        [Column("Password")]
+        public string Password { get; set; }
+        [Column("Disabled")]
+        public bool Disabled { get; set; } = false;
+        [Column("Deleted")]
+        public bool Deleted { get; set; } = false;
+    }
+}
