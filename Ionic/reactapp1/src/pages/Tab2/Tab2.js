bb501e680e376f24ee1ce43fb3cf3027d6328057
Peter@Desktop6700 <peter@sitedisks.com.au>
2019-11-27 21:39:45 +1100

    IonCard updates

--- Ionic/reactapp1/src/pages/Tab2/Tab2.js
+++ Ionic/reactapp1/src/pages/Tab2/Tab2.js
@@ -2,29 +2,71 @@ import React from 'react';
 import {
     IonContent,
     IonToolbar,
-    IonTitle, 
-    IonHeader, 
+    IonTitle,
+    IonHeader,
     IonPage,
+    IonIcon,
     IonList,
-    IonTabBar, IonItem, IonLabel
+    IonItem, IonLabel, IonCard, IonCardTitle, IonCardContent, IonCardSubtitle, IonCardHeader
 } from '@ionic/react';
+import { config } from '@ionic/core';
+import { CONFIG } from '../constants';
 
 class Tab2 extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            news: []
+        };
+    }
+
+    componentDidMount() {
+        // example: https://newsapi.org/v2/top-headlines?country=cn&category=technology&page=2&apiKey=60b923e5e58c4b9d88122c0a9d2ad60b
+        fetch(CONFIG.API_ENDPOINT + '?country=cn&category=technology&page=1&apiKey=' + CONFIG.API_KEY)
+            .then(res => res.json())
+            .then(
+                (res) => {
+                    this.setState({
+                        news: res.articles
+                    });
+                },
+                (err) => {
+                    console.error(err);
+                }
+            );
+    }
 
     render() {
         return (
             <IonPage>
                 <IonHeader>
                     <IonToolbar color="primary">
-                        <IonTitle>Tab Two</IonTitle>
+                        <IonTitle>News</IonTitle>
                     </IonToolbar>
                 </IonHeader>
                 <IonContent>
+
                     <IonList>
                         <IonItem routerLink="/tab2/details">
+                        <IonIcon name="wifi" slot="start" />
                             <IonLabel>Go to detail</IonLabel>
                         </IonItem>
                     </IonList>
+
+                    {this.state.news.map((article, index) =>
+                        <IonCard key={index}>
+                            <img src={article.urlToImage} />
+                            <IonCardHeader>
+                                <IonCardSubtitle>{article.publishedAt}</IonCardSubtitle>
+                                <IonCardTitle>{article.title}</IonCardTitle>
+                            </IonCardHeader>
+                            <IonCardContent>
+                                {article.description}
+                            </IonCardContent>
+                        </IonCard>
+                    )
+                    }
                 </IonContent>
             </IonPage>
         );
---
+++ Ionic/reactapp1/src/pages/constants.js
@@ -0,0 +1,5 @@
+export const CONFIG = {
+    API_KEY: '60b923e5e58c4b9d88122c0a9d2ad60b',
+    API_ENDPOINT: 'https://newsapi.org/v2/top-headlines',
+    // example: https://newsapi.org/v2/top-headlines?country=cn&category=technology&page=2&apiKey=60b923e5e58c4b9d88122c0a9d2ad60b
+};
