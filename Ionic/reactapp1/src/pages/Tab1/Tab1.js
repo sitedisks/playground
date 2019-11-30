95b9433b09be9bad448e68a941b34bc773f220b5
Peter@Dell <peter@sitedisks.com.au>
2019-11-30 14:56:07 +1100

    capacitor camera!!

--- Ionic/reactapp1/src/pages/Tab1/Tab1.js
+++ Ionic/reactapp1/src/pages/Tab1/Tab1.js
@@ -1,19 +1,57 @@
 import React from 'react';
-import { IonContent, IonToolbar, IonTitle, IonHeader, IonPage } from '@ionic/react';
+import {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonImg,
+    IonFab,
+    IonFabButton,
+    IonIcon
+} from '@ionic/react';
+import { Plugins, CameraResultType } from '@capacitor/core';
 import './Tab1.css';
 
+const { Camera } = Plugins;
+
 class Tab1 extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { photo: '' };
+    }
+
+    async takePicture() {
+        console.log('button clicked');
+        const image = await Camera.getPhoto({
+
+            quality: 90,
+            allowEditing: false,
+            resultType: CameraResultType.Uri
+        });
+
+        var imageUrl = image.webPath;
+
+        //can be set to the src of an image now
+        this.setState({ photo: imageUrl});
+    };
 
     render() {
+        const photo = this.state.photo;
         return (
             <IonPage>
                 <IonHeader>
-                    <IonToolbar color="primary">
-                        <IonTitle>Tab One</IonTitle>
+                    <IonToolbar>
+                        <IonTitle>Ionic Camera</IonTitle>
                     </IonToolbar>
                 </IonHeader>
                 <IonContent>
-                    <code>This is so cool!</code>
+                    <IonImg src={photo} style={{ 'border': '1px solid black', 'minHeight': '100px' }} />
+                    <IonFab color="primary" vertical="bottom" horizontal="center" slot="fixed">
+                        <IonFabButton color="primary" onClick={() => this.takePicture()}>
+                            <IonIcon name="add" />
+                        </IonFabButton>
+                    </IonFab>
                 </IonContent>
             </IonPage>
         );
