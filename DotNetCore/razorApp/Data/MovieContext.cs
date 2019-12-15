61aeceac192dd53e96f99a3f84b94325a08975d7
Peter@Desktop6700 <peter@sitedisks.com.au>
2019-12-15 13:02:27 +1100

    ignore .vs

---
+++ DotNetCore/razorApp/Data/MovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using razorApp.Models;
+
+namespace razorApp.Data
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext (DbContextOptions<MovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<razorApp.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ DotNetCore/razorApp/Pages/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace razorApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Message;
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Message = "Page in C# Razoer";
+
+        }
+
+        public async Task<IActionResult> OnPost() {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
