6231943e7af8430caad438177d0889426a482fde
Peter@Desktop6700 <peter@sitedisks.com.au>
2020-02-19 22:48:08 +1100

    >ng g m modules/header

--- Angular8/mac-app1/src/app/app.module.ts
+++ Angular8/mac-app1/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { HeaderModule } from './modules/header/header.module';
 import { environment } from '../environments/environment';
+
+import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { DepartmentListComponent } from './department-list/department-list.component';
@@ -19,6 +20,7 @@ import { DepartmentListComponent } from './department-list/department-list.compo
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HeaderModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
