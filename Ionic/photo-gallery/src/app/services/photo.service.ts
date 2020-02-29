130ebf313cc1ca04992e21da4cda7b9015db6e35
Peter@Desktop6700 <peter@sitedisks.com.au>
2020-02-29 21:58:00 +1100

    ionic angular photo-gallery continue the photo service to be continue...

--- Ionic/photo-gallery/src/app/services/photo.service.ts
+++ Ionic/photo-gallery/src/app/services/photo.service.ts
@@ -1,5 +1,13 @@
 import { Injectable } from '@angular/core';
-import { Plugins, CameraResultType, Capacitor, FilesystemDirectory, CameraPhoto, CameraSource } from '@capacitor/core';
+import {
+  Plugins,
+  CameraResultType,
+  Capacitor,
+  FilesystemDirectory,
+  CameraPhoto,
+  CameraSource,
+  WebPlugin
+} from '@capacitor/core';
 import { Photo } from './photo';
 
 const { Camera, Filesystem, Storage } = Plugins;
@@ -22,10 +30,35 @@ export class PhotoService {
       quality: 100
     });
 
+    const savedImageFile = await this.savePicture(capturedPhoto);
+
     this.photos.unshift({
       filepath: "soon...",
       webviewPath: capturedPhoto.webPath
     });
   }
+
+  private async savePicture(cameraPhoto: CameraPhoto) {
+
+    const base64Data = await this.readAsBase64(cameraPhoto);
+
+  }
+
+  private async readAsBase64(cameraPhoto: CameraPhoto) {
+    // fetch the photo, read as a blob, then convert to base64 format
+    const response = await fetch(cameraPhoto.webPath);
+    const blob = await response.blob();
+
+    return await this.convertBlobToBase64(blob) as string;
+  }
+
+  convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject)=>{
+    const reader = new FileReader;
+    reader.onerror = reject;
+    reader.onload =()=> {
+      resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
 
 }
