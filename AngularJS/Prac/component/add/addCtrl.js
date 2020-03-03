3480c25c1da1d909e38570ed903b5418bab392d1
Peter@Desktop6700 <peter@sitedisks.com.au>
2020-03-04 09:26:10 +1100

    https://docs.angularjs.org/guide/unit-testing

--- AngularJS/Prac/component/add/addCtrl.js
+++ AngularJS/Prac/component/add/addCtrl.js
@@ -3,5 +3,23 @@
 
     app.controller('componentAddCtrl', ['$scope', function($scope){
         $scope.message = "component add";
+        $scope.password = '';
+
+        $scope.grade = grade;
+
+        function grade() {
+            var size= $scope.password.length;
+            if(size > 8){
+                $scope.strength = 'strong';
+
+            }else if (size > 3) {
+                $scope.strength = 'medium';
+
+            }
+            else {
+                $scope.strength = 'weak';
+            }
+        }
+
     }]);
 })();
---
+++ AngularJS/Prac/test.js
@@ -0,0 +1,5 @@
+describe('Same test', function(){
+    it('Condition is true', function(){
+        expect('AngularJS').toBe('AngularJS');
+    })
+})
