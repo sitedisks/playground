a278b10a33f227bc260eaed0f99bc71f31d3b2cf
Peter@Desktop6700 <peter@sitedisks.com.au>
2020-03-03 12:45:25 +1100

    angular todo 你需要学习整个结构 并吃透其中用到的所有知识

--- Angular8/ng8-pre/src/app/app-routing.module.ts
+++ Angular8/ng8-pre/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { ExampleComponent } from './example/example.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'example', component: ExampleComponent },
 ];
 
 @NgModule({
