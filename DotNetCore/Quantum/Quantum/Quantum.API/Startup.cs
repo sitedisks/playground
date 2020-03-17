396cbdafc50f7415a561bc26de4407ad6081ca5a
Peter@Desktop6700 <peter@sitedisks.com.au>
2020-03-17 23:11:11 +1100

    Quantum add swagger

--- DotNetCore/Quantum/Quantum/Quantum.API/Startup.cs
+++ DotNetCore/Quantum/Quantum/Quantum.API/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Hosting;
 using Quantum.API.Data;
 using Quantum.API.Interfaces;
 using Quantum.API.Services;
+using Microsoft.OpenApi.Models;
 
 namespace Quantum.API
 {
@@ -27,17 +28,31 @@ namespace Quantum.API
                 options.UseSqlServer(Configuration.GetConnectionString("LocalDBConnection")));
             services.AddTransient<IClassService, ClassService>();
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Quantum API", Version = "v1", Description="Quantum RESTFul WebAPI" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseCors(builder =>
+                builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+
             app.UseHttpsRedirection();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Quantum API V1.0"));
 
             app.UseRouting();
 
