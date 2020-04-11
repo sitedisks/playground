98bb3c3e6eff92aa2de31626a9ebf205d7108268
Peter@ROG <peter@sitedisks.com.au>
2020-04-11 16:47:13 +1000

    angular share button directive

--- Angular8/charts-app/src/app/app.module.ts
+++ Angular8/charts-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { ShareModule } from '@ngx-share/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { StatusComponent } from './status/status.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ShareModule
   ],
   providers: [],
   bootstrap: [AppComponent]
