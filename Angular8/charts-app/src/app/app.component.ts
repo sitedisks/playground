554ff2b4bac099cf9a4ac4c47c5d81fbf1cfceef
Peter@ROG <peter@sitedisks.com.au>
2020-04-11 16:26:16 +1000

    angular charts-app clean up

--- Angular8/charts-app/src/app/app.component.ts
+++ Angular8/charts-app/src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Data } from './model/data.interface';
-import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-root',
@@ -10,45 +7,10 @@ import { Chart } from 'chart.js';
 })
 export class AppComponent implements OnInit {
   title = 'charts-app';
-  data: Data[];
-  url = 'http://localhost:4000/results';
-  month = [];
-  price = [];
-  chart = [];
 
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
   ngOnInit() {
 
-    this.http.get(this.url).subscribe(
-      (result: Data[]) => {
-        result.forEach(y => {
-          this.month.push(y.month);
-          this.price.push(y.price);
-        });
-
-        this.chart = new Chart('ppp', {
-          type: 'line',
-          data: {
-            labels: this.month,
-            datasets: [{
-              label: 'Crude oil prices',
-              data: this.price,
-              borderColor: '#ooff33',
-              // backgroundColor: '00ff00',
-              fill: true
-            }]
-          },
-          options: {
-            // responsive: true,
-            legend: { display: true },
-            scales: {
-              xAxes: [{ display: true }],
-              yAxes: [{ display: true }]
-            }
-          }
-        });
-      }
-    );
   }
 }
