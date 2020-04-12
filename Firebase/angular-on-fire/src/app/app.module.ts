7242603c5c49b649ec5435e423070fe1c22a976c
Peter@ROG <peter@sitedisks.com.au>
2020-04-12 10:10:01 +1000

    Firebase + Angular $ng add @angular/material

--- Firebase/angular-on-fire/src/app/app.module.ts
+++ Firebase/angular-on-fire/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OrdersComponent } from './orders/orders.component';
 import { OrderListComponent } from './order-list/order-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { OrderListComponent } from './order-list/order-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
