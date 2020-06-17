102c43f0b1b08789da0021efab3d496520bdd833
Peter@ROG <peter@sitedisks.com.au>
2020-06-17 22:48:07 +1000

    no message

--- NodeJS/myapp/app.js
+++ NodeJS/myapp/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.port || 3000;
 
  app.get('/', (req, res)=>{
-     res.send('Hello World');
+     res.send('PinPiece Welcome to the real world!');
  });
 
  app.listen(port, ()=>{
