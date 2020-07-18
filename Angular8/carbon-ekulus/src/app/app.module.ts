057672b1d0e6731c96c7f3828ee30a8aa47b529a
Peter@ROG <peter@sitedisks.com.au>
2020-07-18 12:36:05 +1000

    Clear install of the Carbon design system.

--- Angular8/carbon-ekulus/src/app/app.module.ts
+++ Angular8/carbon-ekulus/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
+// carbon-components-angular default imports
+import { UIShellModule } from 'carbon-components-angular';
+ 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		HeaderComponent
 	],
 	imports: [
 		BrowserModule,
-		AppRoutingModule
+		AppRoutingModule,
+		UIShellModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
