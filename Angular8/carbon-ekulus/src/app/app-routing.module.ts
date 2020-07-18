9012e75f8a680085f155a1f9c01c0ea86609369e
Peter@ROG <peter@sitedisks.com.au>
2020-07-18 12:47:26 +1000

    Clear install Carbon just home page

--- Angular8/carbon-ekulus/src/app/app-routing.module.ts
+++ Angular8/carbon-ekulus/src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		loadChildren: ()=> import('./home/home.module').then((m)=>m.HomeModule)
+	}
+];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
+	imports: [RouterModule.forRoot(routes, {useHash: true, enableTracing: false})],
 	exports: [RouterModule]
 })
 export class AppRoutingModule { }
