ca1bec28450cbac216f42cfea535ca1879ac335b
Peter Wang <peter@sitedisks.com.au>
2021-07-13 11:59:46 +1000

    nodeJS - hello eBrochure

--- NodeJS/hello/app.js
+++ NodeJS/hello/app.js
@@ -1,2 +1,22 @@
-var msg = "Hello World";
-console.log(msg);
+var express = require('express')
+var app = express()
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+    res.send('hello world')
+})
+
+app.route('/book')
+    .get(function (req, res) {
+        res.send('Get a random book')
+    })
+    .post(function (req, res) {
+        res.send('Add a book')
+    })
+    .put(function (req, res) {
+        res.send('Update the book')
+    })
+    
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
